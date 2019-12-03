3e90e18716967c1c22d0d49f974131f0f131ceaf
SEVERINECP <sevpicot@gmail.com>
2019-12-03 14:41:23 +0100

    PHPPartie1Exo6

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ $number=140;
+ ?>
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHPPartie1Exo6</title>
+  </head>
+  <body>
+    <p><?php
+      echo $number;
+      ?>
+    </p>
+    <?php
+      echo $number + 30;
+      ?>
+    <p>
+      <?php
+        echo ($number + 30) / 2;
+        ?>
+    </p>
+  </body>
+</html>
